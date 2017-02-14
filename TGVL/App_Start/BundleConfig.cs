f77f1ce2ce25ab71992aeebcb7b0ef181f3055b0
tungdm <minhtung3131993@gmail.com>
2017-02-14 21:35:06 +0700

    Vy update

--- TGVL/App_Start/BundleConfig.cs
+++ TGVL/App_Start/BundleConfig.cs
@@ -92,11 +92,17 @@ namespace TGVL
                          ));
 
             // VyCMT
-            bundles.Add(new StyleBundle("~/Content/customercss").Include(
-                "~/Content/air.global.2.3.1.min.css",
-                "~/Content/air.components.2.3.1.min.css",
-                "~/Content/homepage.css",
-                "~/Content/components.css",
+            bundles.Add(new StyleBundle("~/Content/mainpagecss").Include(
+                "~/Content/css/font-awesome.css",
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/animate.css",
+                "~/Content/css/style.default.css",
+                "~/Content/css/custom.css"
+             ));
+
+            bundles.Add(new StyleBundle("~/Content/bidrequestcss").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/font-awesome.min.css",
                 "~/Content/nprogress.css",
                 "~/Content/green.css",
                 "~/Content/prettify.min.css",
@@ -104,38 +110,93 @@ namespace TGVL
                 "~/Content/switchery.min.css",
                 "~/Content/starrr.css",
                 "~/Content/daterangepicker.css",
-                "~/Content/custom.min.css",
+                "~/Content/custom.min.css"
+                ));
+            bundles.Add(new StyleBundle("~/Content/productcss").Include(
                 "~/Content/bootstrap.min.css",
                 "~/Content/font-awesome.min.css",
-                "~/Content/css/animate.css",
-                "~/Content/css/style.default.css",
-                "~/Content/css/custom.css"
+                "~/Content/nprogress.css",
+                "~/Content/green.css",
+                "~/Content/dataTables.bootstrap.min.css",
+                "~/Content/buttons.bootstrap.min.css",
+                "~/Content/fixedHeader.bootstrap.min.css",
+                "~/Content/responsive.bootstrap.min.css",
+                "~/Content/scroller.bootstrap.min.css",
+                "~/Content/custom.min.css"
                 ));
-            bundles.Add(new ScriptBundle("~/bundles/customerjquery").Include(
-                                    "~/Scripts/bootstrap.min.js",
-                                   "~/Scripts/js/jquery.cookie.js",
+
+            //bundles.Add(new StyleBundle("~/Content/wizardcss").Include(
+            //    "~/Content/bootstrap.min.css",
+            //    "~/Content/font-awesome.min.css",
+            //    "~/Content/nprogress.css",
+            //    "~/Content/custom.min.css"
+            //    ));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/mainpagejquery").Include(
+                                    "~/Scripts/js/jquery-1.11.0.min.js",
+                                    "~/Scripts/js/bootstrap.min.js",
+                                    "~/Scripts/js/jquery.cookie.js",
                                     "~/Scripts/js/waypoints.min.js",
                                     "~/Scripts/js/jquery.counterup.min.js",
                                     "~/Scripts/js/jquery.parallax-1.1.3.js",
                                     "~/Scripts/js/front.js",
-                                    "~/Scripts/custom.min.js",
+                                    "~/Scripts/js/owl.carousel.min.js"
+                                    ));
+
+            bundles.Add(new ScriptBundle("~/bundles/bidrequestjquery").Include(
+                                   "~/Scripts/jquery.min.js",
+                                    "~/Scripts/bootstrap.min.js",
                                     "~/Scripts/fastclick.js",
                                     "~/Scripts/nprogress.js",
-                                    "~/Scripts/moment.min.js",
-                                    "~/Scripts/icheck.min.js",
                                     "~/Scripts/bootstrap-progressbar.min.js",
-                                    "~/Scripts/bootstrap-wysiwyg.min.js",
+                                    "~/Scripts/icheck.min.js",
+                                    "~/Scripts/moment.min.js",
                                     "~/Scripts/daterangepicker.js",
-                                    "~/Scripts/prettify.js",
+                                    "~/Scripts/bootstrap-wysiwyg.min.js",
                                     "~/Scripts/jquery.hotkeys.js",
-                                    "~/Scripts/switchery.min.js",
+                                    "~/Scripts/prettify.js",
                                     "~/Scripts/jquery.tagsinput.js",
+                                    "~/Scripts/switchery.min.js",
                                     "~/Scripts/select2.full.min.js",
                                     "~/Scripts/autosize.min.js",
                                     "~/Scripts/jquery.autocomplete.min.js",
                                     "~/Scripts/starrr.js",
                                     "~/Scripts/custom.min.js"
                                     ));
+
+            bundles.Add(new ScriptBundle("~/bundles/productjquery").Include(
+                                    "~/Scripts/jquery.min.js",
+                                    "~/Scripts/bootstrap.min.js",
+                                    "~/Scripts/fastclick.js",
+                                    "~/Scripts/nprogress.js",
+                                    "~/Scripts/icheck.min.js",
+                                    "~/Scripts/jquery.dataTables.min.js",
+                                    "~/Scripts/dataTables.bootstrap.min.js",
+                                    "~/Scripts/dataTables.buttons.min.js",
+                                    "~/Scripts/buttons.bootstrap.min.js",
+                                    "~/Scripts/buttons.flash.min.js",
+                                    "~/Scripts/buttons.html5.min.js",
+                                    "~/Scripts/buttons.print.min.js",
+                                    "~/Scripts/dataTables.fixedHeader.min.js",
+                                    "~/Scripts/dataTables.keyTable.min.js",
+                                    "~/Scripts/dataTables.responsive.min.js",
+                                    "~/Scripts/responsive.bootstrap.js",
+                                    "~/Scripts/dataTables.scroller.min.js",
+                                    "~/Scripts/jszip.min.js",
+                                    "~/Scripts/pdfmake.min.js",
+                                    "~/Scripts/vfs_fonts.js",
+                                    "~/Scripts/jquery.smartWizard.js",
+                                    "~/Scripts/custom.min.js"
+                                    ));
+            //bundles.Add(new ScriptBundle("~/bundles/wizardjquery").Include(
+            //                        "~/Scripts/jquery.min.js",
+            //                        "~/Scripts/bootstrap.min.js",
+            //                        "~/Scripts/fastclick.js",
+            //                        "~/Scripts/nprogress.js",
+            //                        "~/Scripts/jquery.smartWizard.js",
+            //                        "~/Scripts/custom.min.js"
+            //                        ));
         }
     }
 }
